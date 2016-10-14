fee911c3415d3b36bf61192be5cbc6af8f25a2d3
Bérénice Batut <bebatut@gmail.com>
2016-10-14 10:57:46 +0200

    Change plot size and correct some variable names

--- src/manipulate_training_stats.R
+++ src/manipulate_training_stats.R
@@ -1,7 +1,7 @@
 training_stats = read.csv("data/training_stats.csv", sep = ";", h = T)
 
 # Manipulate the date to extract number of workshops per month
-date = as.factor(paste(data$year, data$month, "1", sep="/"))
+date = as.factor(paste(training_stats$year, training_stats$month, "1", sep="/"))
 seq_date = seq(as.Date(date[length(date)]), as.Date(date[1]), by="month")
 
 tmp_ws_stat_per_month = summary(date)
@@ -13,15 +13,15 @@ for(t in names(tmp_ws_stat_per_month)){
     ws_stat_per_month[t] = tmp_ws_stat_per_month[t]
 }
 
-png("results/workshop_per_month.png", width = 560, height = 480)
-par(mar=c(7,3,1,1), pty = "m", bty = "l")
-plot(ws_stat_per_month, type = "l", ylab = "", xlab = "", col = "blue4", xaxt = "n")
-axis(1, at=seq(1, length(ws_stat_per_month)),labels = FALSE)
-
-to_label = seq(1, length(ws_stat_per_month), by = 4)
+to_label = seq(1, length(ws_stat_per_month), by = 3)
 labels = format(as.Date(names(ws_stat_per_month)[to_label]), "%B %Y")
-text(to_label + 1.5, par("usr")[3] - .4,  labels = labels, srt = 45, pos = 2, xpd = TRUE)
+
+png("images/workshop_per_month.png", width = 1000, height = 750)
+par(mar=c(7,3,1,1), pty = "m", bty = "l")
+plot(ws_stat_per_month, type = "l", ylab = "", xlab = "", col = "blue4", xaxt = "n", cex.axis = 1.2, lwd = 2)
+axis(1, at=seq(1, length(ws_stat_per_month)),labels = FALSE)
+text(to_label + .75, par("usr")[3] - .25,  labels = labels, srt = 45, pos = 2, xpd = TRUE, cex = 1.2)
 dev.off()
 
 # Extract cities and number of workshop per cities
-cities = paste(data$city, data$country, sep=", ")
+cities = paste(training_stats$city, training_stats$country, sep=", ")
